d81cdf41dab7395d7d5b1288f7f2830243dc0021
unknown <onildobr@gmail.com>
2019-08-19 12:39:56 -0300

    terceiro comite

--- app/src/main/java/com/example/apptrabalho01/MainActivity.kt
+++ app/src/main/java/com/example/apptrabalho01/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.apptrabalho01
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_tela2.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,66 +17,96 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var params = intent.extras
-        var texto = params?.getString("TEXTO")
-        resp1.setText(texto)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Activity.RESULT_OK
+                val param = data?.extras
+                val R1 = param?.getString("TEXTO")
+                resp1.setText(R1)
+            }
+        } else {
+            if (requestCode == 2) {
+                if (resultCode == RESULT_OK) {
+                    Activity.RESULT_OK
+                    val param = data?.extras
+                    val R2 = param?.getString("TEXTO")
+                    resp2.setText(R2)
+                }
+            } else {
+                if (requestCode == 3) {
+                    if (resultCode == RESULT_OK) {
+                        Activity.RESULT_OK
+                        val param = data?.extras
+                        val R3 = param?.getString("TEXTO")
+                        resp3.setText(R3)
+                    }
+                }
+            }
+        }
     }
 
     fun btn1(view: View) {
-
-        var A = "5"
-
+        val MY_RESULT_CODE = 99
+        var A = R.drawable.img2
         var i = Intent(this, Tela2::class.java)
         var b = Bundle()
-        //var mensagem = R.drawable.img1
-        b.putString("TEXTO", A)
+        b.putInt("TEXTO", A)
         i.putExtras(b)
-        startActivity(i)
+        startActivityForResult(i, 1)
     }
 
     fun btn2(view: View) {
 
-        var A = "6"
-
+        val MY_RESULT_CODE = 99
+        var B = R.drawable.img3
         var i = Intent(this, Tela2::class.java)
         var b = Bundle()
-        //var mensagem = R.drawable.img1
-        b.putString("TEXTO", A)
+        b.putInt("TEXTO", B)
         i.putExtras(b)
-        startActivity(i)
+        startActivityForResult(i, 2)
     }
 
     fun btn3(view: View) {
 
-        var A = "8"
-
+        val MY_RESULT_CODE = 99
+        var C = R.drawable.img4
         var i = Intent(this, Tela2::class.java)
         var b = Bundle()
-        //var mensagem = R.drawable.img1
-        b.putString("TEXTO", A)
+        b.putInt("TEXTO", C)
         i.putExtras(b)
-        startActivity(i)
+        startActivityForResult(i, 3)
     }
 
     fun btnverificar(view: View) {
 
-        //var Resposta1 : String = resp1.text.toString();
-        //var Resposta2 : String = resp2.text.toString();
-        //var Resposta3 : String = resp3.text.toString();
+        var Resposta1: String = resp1.text.toString();
+        var Resposta2: String = resp2.text.toString();
+        var Resposta3: String = resp3.text.toString();
 
-        
 
-        var A = "verdadeiro"
-        var B = "Falso"
+        var A = "Paciente não é Daltônico"
+        var B = "Paciente é Daltônico"
 
-        if (resp1.equals("5")) {
+
+        if (Resposta1.equals("29") && Resposta2.equals("74") && Resposta3.equals("2")) {
             //Toast.makeText(this, A,   Toast.LENGTH_SHORT).show()
             result.setText(A)
-        } else
-        //Toast.makeText(this, B,   Toast.LENGTH_SHORT).show()
+        } else {
             result.setText(B)
+        }
+
     }
 
+    fun btnLimpar (view: View) {
+        resp1.text.clear()
+        resp2.text.clear()
+        resp3.text.clear()
+        result.text.clear()
+        }
 
-    }
+}
 
--- app/src/main/java/com/example/apptrabalho01/Tela2.kt
+++ app/src/main/java/com/example/apptrabalho01/Tela2.kt
@@ -1,5 +1,6 @@
 package com.example.apptrabalho01
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
@@ -7,35 +8,43 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.example.apptrabalho01.R.drawable.img1
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_tela2.*
 
 class Tela2 : AppCompatActivity() {
 
+   
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
 
+
         var params = intent.extras
-        var texto = params?.getString("TEXTO")
-        numtxt.setText(texto)
+        var imagem = params?.getInt("TEXTO")
+        if (imagem != null) {
+            img1.setImageResource(imagem)
+        }
     }
 
-    fun btnok (view: View) {
-        var i = Intent(this, MainActivity::class.java)
-        var b = Bundle()
+    fun btnok(view: View) {
+
+        var i = Intent(this, Tela2::class.java)
         var mensagem = dgtText.text.toString()
-        b.putString("TEXTO", mensagem)
-        i.putExtras(b)
-        startActivity(i)
+        i.putExtra("TEXTO", mensagem)
+        setResult(Activity.RESULT_OK, i)
+        finish()
     }
+    fun btncancelar(view: View) {
 
+            var i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
 
-    fun btncancelar (view: View) {
-        var i = Intent(this, MainActivity::class.java)
-        startActivity(i)
-    }
 }
+
+
 
 
 
